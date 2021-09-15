ca3566443a4604cce320e8a6bc685a8de5e092f6
Chris <chrisolo2001@gmail.com>
2021-09-14 20:31:09 -0500

    Python Fundamentals

---
+++ cc1.py
@@ -0,0 +1,17 @@
+# Turn this line into a comment
+# cities = ["Seattle", "Tacoma", "Bellevue"]
+# a = 1
+# print(type(a))
+# b = 5.0
+# print(type(b))
+# c = 'chris'
+# print(type(c))
+# d = a + b
+
+n = 5 
+while n > 1:
+      print(n)
+      n = n -1
+      if n == 2:
+          break
+print(n)
